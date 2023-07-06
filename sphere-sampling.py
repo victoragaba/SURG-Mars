3b2f19f5163c4cb25d96b8a28605e1818a4b50a6
Victor Agaba <112359622+VictorAgaba@users.noreply.github.com>
2023-07-06 17:36:35 -0500

    random sampling hemisphere

--- sphere-sampling.py
+++ sphere-sampling.py
@@ -1,15 +1,54 @@
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.stats as stats
 
-n = 1000
+def set_axes_equal(ax): # might be useful later
+    """
+    Make axes of 3D plot have equal scale so that spheres appear as spheres,
+    cubes as cubes, etc.
+
+    Input
+      ax: a matplotlib axis, e.g., as output from plt.gca().
+    """
+
+    x_limits = ax.get_xlim3d()
+    y_limits = ax.get_ylim3d()
+    z_limits = ax.get_zlim3d()
+
+    x_range = abs(x_limits[1] - x_limits[0])
+    x_middle = np.mean(x_limits)
+    y_range = abs(y_limits[1] - y_limits[0])
+    y_middle = np.mean(y_limits)
+    z_range = abs(z_limits[1] - z_limits[0])
+    z_middle = np.mean(z_limits)
+
+    # The plot bounding box is a sphere in the sense of the infinity
+    # norm, hence I call half the max range the plot radius.
+    plot_radius = 0.5*max([x_range, y_range, z_range])
+
+    ax.set_xlim3d([x_middle - plot_radius, x_middle + plot_radius])
+    ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
+    ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
+
+N = 5000
 samples = []
 
-for i in range(n):
-    v = stats.norm.rvs(size=3)
-    while np.linalg.norm(v) < 0.00001:
-        v = stats.norm.rvs(size=3)
-    v /= np.linalg.norm(v)
-    samples.append(v)
+for i in range(N):
+    p = stats.norm.rvs(size=3)
+    while np.linalg.norm(p) < 0.00001:
+        p = stats.norm.rvs(size=3)
+    p /= np.linalg.norm(p)
+    p[2] = abs(p[2]) # only upper hemisphere
+    samples.append(p)
     
-fig = plt.figure()
+plt.figure()
+ax = plt.axes(projection='3d')
+fig = ax.scatter3D([p[0] for p in samples],
+                   [p[1] for p in samples],
+                   [p[2] for p in samples], c='b', s=1)
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+plt.show()
